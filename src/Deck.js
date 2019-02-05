610f25c5a9965840c3b39272eaaa45d8e499ebcf
Orhan Erbas <orhanerba@icloud.com>
2019-02-05 20:50:25 +0300

    swipe completion

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
     constructor(props){
@@ -10,14 +14,42 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
-            onPanResponderRelease: () => {}
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD){
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD){
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
+            }
         });
         this.state = { panResponder, position };
+    }
+
+    forceSwipe(direction){
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: { x, y: 0  },
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction){
+        const { onSwipeLeft, onSwipeRight } = this.props;
+
+        direction === 'right' ? onSwipeRight() : onSwipeLeft();
+    }
+
+    resetPosition(){
+        Animated.spring(this.state.position, {
+            toValue: { x:0, y:0 }
+        }).start();
     }
     getCardStyle (){
         const { position } = this.state;
         const rotate = position.x.interpolate({
-           inputRange: [-500, 0, 500],
+           inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
            outputRange: ['-120deg', '0deg', '120deg']
         });
         return {
