2e7a298021f48e98420e24b87c4bfe6ca482beae
Orhan Erbas <orhanerba@icloud.com>
2019-02-24 01:07:39 +0300

    cards to stack

--- src/Deck.js
+++ src/Deck.js
@@ -77,15 +77,19 @@ class Deck extends Component {
                 return (
                     <Animated.View
                         key={item.id}
-                        style={this.getCardStyle()}
+                        style={[this.getCardStyle(), styles.cardStyle]}
                         {...this.state.panResponder.panHandlers}
                     >
                         {this.props.renderCard(item)}
                     </Animated.View>
                 );
             }
-            return this.props.renderCard(item);
-        });
+            return (
+                <View key={item.id} style={styles.cardStyle}>
+                    {this.props.renderCard(item)}
+                </View>
+            );
+        }).reverse();
     }
     render() {
         return (
@@ -96,5 +100,13 @@ class Deck extends Component {
         );
     }
 }
+
+const styles = {
+    cardStyle: {
+        position: 'absolute',
+        width: SCREEN_WIDTH
+
+    }
+};
 
 export default Deck;
